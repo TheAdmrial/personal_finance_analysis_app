3a7320314abf307bfa16efa243d3742e0c06e69d
TheAdmrial <adamromriell@gmail.com>
2024-09-04 22:05:16 -0600

    inserting testing data

--- SQL/testing_inserting_data.sql
+++ SQL/testing_inserting_data.sql
@@ -1,12 +1,20 @@
 INSERT INTO transaction_type (id, type_name) VALUES 
-(DEFAULT, 'testing1')
-,(DEFAULT, 'testing 2')
-,(DEFAULT, 'testing3')
-,(DEFAULT, '4testing');
+-- (DEFAULT, 'testing1')
+-- ,(DEFAULT, 'testing 2')
+-- ,(DEFAULT, 'testing3')
+-- ,(DEFAULT, '4testing')
+-- ,
+(DEFAULT, 'Income');
 
-SELECT * from transaction_type;
+SELECT * FROM transaction_type;
 
-INSERT INTO company (id, company_name) VALUE
-(DEFAULT, '1testing')
-, (DEFAULT, '2 testing')
-, (DEFAULT, '3  testing')
+SELECT * FROM company;
+
+INSERT INTO company (id, company_name) VALUES
+-- (DEFAULT, '1testing')
+-- , (DEFAULT, '2 testing')
+-- , (DEFAULT, '3  testing')
+-- , 
+(DEFAULT, 'Ftbl Co');
+
+SELECT * FROM company;
