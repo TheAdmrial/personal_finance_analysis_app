461e5630e73ed6ac556da3fd459bcb80c97fdc8d
TheAdmrial <adamromriell@gmail.com>
2024-09-17 20:44:24 -0600

    removing and readding in the testing data to the database

--- SQL/testing_inserting_data.sql
+++ SQL/testing_inserting_data.sql
@@ -3,8 +3,7 @@ INSERT INTO transaction_type (id, type_name) VALUES
 -- ,(DEFAULT, 'testing 2')
 -- ,(DEFAULT, 'testing3')
 -- ,(DEFAULT, '4testing')
--- ,
-(DEFAULT, 'Income');
+-- ,(DEFAULT, 'Income');
 
 SELECT * FROM transaction_type;
 
@@ -14,7 +13,6 @@ INSERT INTO company (id, company_name) VALUES
 -- (DEFAULT, '1testing')
 -- , (DEFAULT, '2 testing')
 -- , (DEFAULT, '3  testing')
--- , 
-(DEFAULT, 'Ftbl Co');
+-- , (DEFAULT, 'Ftbl Co');
 
 SELECT * FROM company;
