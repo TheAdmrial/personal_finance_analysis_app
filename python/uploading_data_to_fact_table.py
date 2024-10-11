4c61439d6042bc74b6c3670f45d6d0dd57c25b21
TheAdmrial <adamromriell@gmail.com>
2024-10-10 21:15:00 -0600

    testing out loading the data into the database

--- python/uploading_data_to_fact_table.py
+++ python/uploading_data_to_fact_table.py
@@ -3,7 +3,7 @@ import polars as pl
 import os
 from dotenv import load_dotenv, dotenv_values
 import psycopg2
-from uploading_data_to_dim_tables import get_connection, get_existing_categories, get_existing_companies
+from uploading_data_to_dim_tables import get_existing_categories, get_existing_companies, get_connection
 #%%
 # the postgresql db info
 # TODO: Where and when to add try-catch statements
@@ -27,12 +27,6 @@ test_port = str(os.environ.get("fin_app_test_port"))
 '''
 #%%
 ##### Getting the category and company data #####
-'''Most likely I don't need to establish a connection. I just need the uri to make the get and write to the database.'''
-# conn = get_connection()
-# if conn:
-#     print('Connection to the PostgreSQL established successfully.')
-# else:
-#     print('Connection to the PostgreSQL encountered an error.')
 # making the uri for the get functions
 uri = "postgresql://%s:%s@%s:%s/%s" % (test_user, test_pass,test_server,test_port, test_db)
 #%%
@@ -55,14 +49,30 @@ with_categories = semi_clean_data.join(other = categories
 # %%
 # rename the new id column to type_id
 # drop the category string column
+with_categories2 = with_categories.select(
+    pl.col('date')
+    , pl.col('amount')
+    , pl.col('id').alias('type_id')
+    , pl.col('company')
+    , pl.col('description')
+)
 
 #%%
 # join in the company
-
+all_combined = with_categories2.join(other = companies
+                                     , left_on = 'company'
+                                     , right_on= 'company_name'
+                                     , how = 'inner')
 #%%
 # rename the new id column to company_id
 # drop the company string column
-
+all_combined2 = all_combined.select(
+     pl.col('date')
+    , pl.col('amount')
+    , pl.col('type_id')
+    , pl.col('id').alias('company_id')
+    , pl.col('description')
+)
 #%% 
 # rearrage the column order to 
 # 1) date
@@ -70,3 +80,8 @@ with_categories = semi_clean_data.join(other = categories
 # 3) (transaction)type_id (aka category)
 # 4) company_id
 # 5) description
+
+all_combined2.write_database(table_name = 'transactions'
+                             , connection = uri
+                             , engine= 'adbc'
+                             , if_table_exists='replace')
